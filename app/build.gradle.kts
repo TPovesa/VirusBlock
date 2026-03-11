32100c9cbdea13357c66e9a6804014ca37ec29b3
Perdonus <kala5ixa@gmail.com>
2026-03-12 00:36:33 +0300

    Bump Android build version to 18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.shield.antivirus"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.2.5"
+        versionCode = 18
+        versionName = "1.2.6"
     }
 
     signingConfigs {
