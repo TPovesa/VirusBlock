5179ee0d03537348b5102fe8fe1df65422b18b5e
Perdonus <kala5ixa@gmail.com>
2026-03-12 13:12:31 +0300

    Fix Android CI compile error in HomeScreen imports

--- app/src/main/java/com/shield/antivirus/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/shield/antivirus/ui/screens/HomeScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.align
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
