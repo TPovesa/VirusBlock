c0023c97d2c458f9db21d009b3f01c952fa0b9b8
Perdonus <kala5ixa@gmail.com>
2026-03-17 18:20:16 +0300

    Remove published site artifacts from main

--- install/linux.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-exec sh -c 'curl -fsSL https://raw.githubusercontent.com/Perdonus/NV/main/install/nv.sh | sh'
--- install/neuralv.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = 'Stop'
-
-irm https://sosiskibot.ru/neuralv/install/nv.ps1 | iex
-$env:Path = "$env:LOCALAPPDATA\NV;$env:Path"
-nv -v
-nv install @lvls/neuralv
