ee4c3eb3a38e264016b04a2deca541416ba442aa
Perdonus <kala5ixa@gmail.com>
2026-03-18 09:21:19 +0300

    Remove accidental static bundle from repo root

--- install/neuralv.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-curl.exe -fsSL https://sosiskibot.ru/neuralv/install/nv.cmd -o "%TEMP%\nv-install.cmd"
-if errorlevel 1 exit /b 1
-call "%TEMP%\nv-install.cmd"
-if errorlevel 1 exit /b 1
-set "PATH=%LOCALAPPDATA%\NV;%PATH%"
-nv -v
-if errorlevel 1 exit /b 1
-nv install @lvls/neuralv
